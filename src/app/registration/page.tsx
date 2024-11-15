addef18e4eb4d4419ad996eec57870cf4dba7716
Andy Uzal <andyuzal@gmail.com>
2024-11-15 11:35:24 +0000

    feat: Agregando pagina de Registro

---
+++ src/app/registration/page.tsx
@@ -0,0 +1,12 @@
+import RegistrationForm from '@/components/registration-form/RegistrationForm';
+import React from 'react';
+
+function page() {
+  return (
+    <div>
+      <RegistrationForm />
+    </div>
+  );
+}
+
+export default page;
