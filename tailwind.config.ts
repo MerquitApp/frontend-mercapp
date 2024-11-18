456c12d5c60fa803d135de90d289d1f2c5c77a74
Hugo Boyano <hnboyano@gmail.com>
2024-11-18 10:44:26 +0100

    fix tw config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,28 +1,28 @@
-//import type { Config } from "tailwindcss";
-const {nextui} = require("@nextui-org/react");
+import type { Config } from "tailwindcss";
+import { nextui } from '@nextui-org/react';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-        primaryPalette: "#416954",
-        secondaryPalette: "#D9D9D9",
-        redPalette: "#FF0000",
-        whitePalette: "#FFFFFF",
-        greyPalette: "#828282",
-        blackPalette: "#1E1E1E",
-      },
-    },
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+        primaryPalette: '#416954',
+        secondaryPalette: '#D9D9D9',
+        redPalette: '#FF0000',
+        whitePalette: '#FFFFFF',
+        greyPalette: '#828282',
+        blackPalette: '#1E1E1E'
+      }
+    }
   },
-  darkMode: "class",
-  plugins: [nextui()],
+  darkMode: 'class',
+  plugins: [nextui()]
 };
 export default config;
