fc5363277ac90006df1557738c88deaac5fdfdb7
Keibol <jjendo@protonmail.com>
2024-11-21 15:54:23 +0100

    fet:add dropdown

--- src/home/components/Head.tsx
+++ src/home/components/Head.tsx
@@ -12,51 +12,54 @@ import { IoAddCircleOutline } from 'react-icons/io5';
 
 export default function Head() {
   return (
-    <div>
-      <Navbar isBordered>
-        <NavbarBrand>
+    <Navbar isBordered className=" [&>header]:max-w-full justify-center">
+      <Link href="/">
+        <NavbarBrand className="max-w-32">
           <p className="font-bold text-inherit text-2xl text-primaryPalette">
             MercApp
           </p>
         </NavbarBrand>
-        <NavbarContent as="div" className="items-center" justify="end">
-          <Input
-            classNames={{
-              base: 'max-w-full sm:max-w-96 h-10',
-              mainWrapper: 'h-full',
-              input: 'text-small',
-              inputWrapper:
-                'h-full font-normal text-default-500 bg-default-400/20 dark:bg-default-500/20'
-            }}
-            placeholder="Busca tu producto"
-            size="sm"
-            startContent={<PiMagnifyingGlassBold size={18} />}
-            type="search"
-          />
-        </NavbarContent>
-        <NavbarContent justify="end">
-          <NavbarItem className="hidden lg:flex">
-            <Link href="/login">
-              <Button
-                className="text-primaryPalette bg-default-400/20 border-primaryPalette"
-                variant="bordered">
-                Registrate o inicia sesión
-              </Button>
-            </Link>
-          </NavbarItem>
-          <NavbarItem>
-            <Link href="#">
-              <Button
-                className="bg-primaryPalette text-whitePalette"
-                color="primary"
-                startContent={<IoAddCircleOutline size={30} />}
-                variant="flat">
-                Vender
-              </Button>
-            </Link>
-          </NavbarItem>
-        </NavbarContent>
-      </Navbar>
-    </div>
+      </Link>
+      <NavbarContent
+        as="div"
+        className="justify-items-center max-w-screen-lgw-full"
+        justify="end">
+        <Input
+          classNames={{
+            base: 'max-w-full sm:max-w-96 h-10',
+            mainWrapper: 'h-full w-full',
+            input: 'text-small',
+            inputWrapper:
+              'h-full font-normal text-default-500 bg-default-400/20 dark:bg-default-500/20'
+          }}
+          placeholder="Busca tu producto"
+          size="sm"
+          startContent={<PiMagnifyingGlassBold size={18} />}
+          type="search"
+        />
+      </NavbarContent>
+      <NavbarContent justify="end">
+        <NavbarItem className="hidden lg:flex">
+          <Link href="/login">
+            <Button
+              className="text-primaryPalette bg-default-400/20 border-primaryPalette"
+              variant="bordered">
+              Registrate o inicia sesión
+            </Button>
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link href="#">
+            <Button
+              className="bg-primaryPalette text-whitePalette"
+              color="primary"
+              startContent={<IoAddCircleOutline size={30} />}
+              variant="flat">
+              Vender
+            </Button>
+          </Link>
+        </NavbarItem>
+      </NavbarContent>
+    </Navbar>
   );
 }
--- src/home/components/TabsHead.tsx
+++ src/home/components/TabsHead.tsx
@@ -1,14 +1,39 @@
-import { Tabs, Tab } from '@nextui-org/react';
+import { Tabs, Tab, Button } from '@nextui-org/react';
+import {
+  Dropdown,
+  DropdownTrigger,
+  DropdownMenu,
+  DropdownItem
+} from '@nextui-org/dropdown';
 
 export default function App() {
   return (
-    <div className="flex w-full flex-col items-center justify-center gap-2 text-blackPalette">
+    <div className="flex flex-row items-center justify-center gap-2 text-blackPalette pt-4">
+      <Dropdown>
+        <DropdownTrigger>
+          <Button variant="bordered">Todas las categorias</Button>
+        </DropdownTrigger>
+        <DropdownMenu aria-label="Static Actions">
+          <DropdownItem key="ver">Ver todo</DropdownItem>
+          <DropdownItem key="coches">Coches</DropdownItem>
+          <DropdownItem key="motos">Motos</DropdownItem>
+          <DropdownItem key="motor">Motor y accesorios</DropdownItem>
+          <DropdownItem key="informatica">Informática</DropdownItem>
+          <DropdownItem key="deporte">Deporte y ocio</DropdownItem>
+          <DropdownItem key="electrodomesticos">Electrodomésticos</DropdownItem>
+          <DropdownItem key="niños">Niños y bebés</DropdownItem>
+          <DropdownItem key="hogar">Hogar y jardín</DropdownItem>
+          <DropdownItem key="empleo">Empleo</DropdownItem>
+        </DropdownMenu>
+      </Dropdown>
+      {/* Contenido principal */}
+
       <Tabs aria-label="Options" variant="underlined">
-        <Tab key="coches" title="Coches"></Tab>
-        <Tab key="motos" title="Motos"></Tab>
-        <Tab key="motor" title="Motor y accesorios"></Tab>
-        <Tab key="moda" title="Moda y accesorios"></Tab>
-        <Tab key="inmobiliaria" title="Inmobiliaria"></Tab>
+        <Tab key="coches" title="Coches" />
+        <Tab key="motos" title="Motos" />
+        <Tab key="motor" title="Motor y accesorios" />
+        <Tab key="moda" title="Moda y accesorios" />
+        <Tab key="inmobiliaria" title="Inmobiliaria" />
       </Tabs>
     </div>
   );
