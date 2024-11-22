2af5ceb544a155b3bbfb797a4761902811b32367
waxer59 <hnboyano@gmail.com>
2024-11-22 18:40:12 +0100

    add forward ref to input component

--- src/ui/components/Input.tsx
+++ src/ui/components/Input.tsx
@@ -1,4 +1,4 @@
-import type { ReactNode } from 'react';
+import { forwardRef, type ReactNode } from 'react';
 
 interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
   label: string;
@@ -7,25 +7,28 @@ interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
   type?: string;
 }
 
-export default function Input({
-  label,
-  type,
-  name,
-  children,
-  ...props
-}: Props) {
-  return (
-    <div className="flex flex-col gap-2">
-      <label htmlFor={name} className="mt-2">
-        {label}
-      </label>
-      <input
-        name={name}
-        type={type ?? 'text'}
-        className="border-gray-300 border-2 rounded-md p-2"
-        {...props}
-      />
-      {children}
-    </div>
-  );
-}
+export const Input = forwardRef<HTMLInputElement, Props>(
+  ({ label, name, children, type = 'text', ...props }, ref) => {
+    return (
+      <div className="flex flex-col gap-2">
+        <label className="text-grayPalette text-sm font-medium" htmlFor={name}>
+          {label}
+        </label>
+        <div className="flex flex-col gap-1">
+          <input
+            ref={ref}
+            type={type}
+            name={name}
+            className="border-2 border-grayPalette rounded-md p-2"
+            {...props}
+          />
+          {children}
+        </div>
+      </div>
+    );
+  }
+);
+
+Input.displayName = 'Input';
+
+export default Input;
