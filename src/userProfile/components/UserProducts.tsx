f849c5090e3982b8b1c4c1d74b21cc7d7a9d9642
Hugo Boyano <hnboyano@gmail.com>
2025-03-06 13:11:58 +0100

    add basic view

--- src/userProfile/components/UserProducts.tsx
+++ src/userProfile/components/UserProducts.tsx
@@ -1,14 +1,21 @@
-import React from 'react';
 import { ProductResponse } from '@/types';
+import ProductCard from '@/ui/components/ProductCard';
 
 interface Props {
   products: ProductResponse[];
 }
 
-function UserProducts({}: Props) {
+function UserProducts({ products }: Props) {
   return (
     <div className="mt-6">
       <h3 className="text-2xl font-bold mb-3">Productos</h3>
+      <ul className="flex justify-between">
+        {products.map((product) => (
+          <li key={product.id}>
+            <ProductCard product={product} />
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
